a75140e44dba2f8d9f38cfbc09550d2d485a8b85
psywombats <psy_wombats@wombatrpgs.net>
2017-06-11 01:23:39 -0700

    Tilesets first pass complete

---
+++ maps/tilesets/basic.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Basic" tilewidth="32" tileheight="32" tilecount="156" columns="12">
+ <image source="basic.png" width="384" height="416"/>
+ <tile id="0">
+  <properties>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="glyph" value="X"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="glyph" value="@"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="glyph" value="c"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="glyph" value="m"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="glyph" value="n"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="glyph" value="v"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="glyph" value="w"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="glyph" value="W"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="glyph" value="l"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ maps/tilesets/features.tsx
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Features" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="features.png" width="384" height="384"/>
+ <tile id="0">
+  <properties>
+   <property name="glyph" value="}"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="glyph" value="{"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="glyph" value=")"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="glyph" value="("/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="glyph" value="]"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="glyph" value="["/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="glyph" value="&gt;"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="glyph" value="&lt;"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="glyph" value="{"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="glyph" value="B"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="glyph" value="C"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="glyph" value="G"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="glyph" value="T"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="glyph" value="U"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="glyph" value="Y"/>
+  </properties>
+ </tile>
+ <tile id="87">
+  <properties>
+   <property name="glyph" value="I"/>
+  </properties>
+ </tile>
+ <tile id="88">
+  <properties>
+   <property name="glyph" value="A"/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="glyph" value="t"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="glyph" value="+"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="glyph" value="+"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="glyph" value="+"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="glyph" value="+"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="glyph" value="+"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="glyph" value="="/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="glyph" value="="/>
+  </properties>
+ </tile>
+ <tile id="109">
+  <properties>
+   <property name="glyph" value="="/>
+  </properties>
+ </tile>
+ <tile id="110">
+  <properties>
+   <property name="glyph" value="="/>
+  </properties>
+ </tile>
+ <tile id="111">
+  <properties>
+   <property name="glyph" value="="/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="glyph" value="+"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="glyph" value="="/>
+  </properties>
+ </tile>
+ <tile id="124">
+  <properties>
+   <property name="glyph" value="+"/>
+  </properties>
+ </tile>
+ <tile id="125">
+  <properties>
+   <property name="glyph" value="="/>
+  </properties>
+ </tile>
+</tileset>

2a7c084db86fd9253b65b7e789492335fba08c30
psywombats <psy_wombats@wombatrpgs.net>
2017-06-13 01:04:49 -0700

    ftiling up the tilesets

--- maps/tilesets/basic.tsx
+++ maps/tilesets/basic.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Basic" tilewidth="32" tileheight="32" tilecount="156" columns="12">
- <image source="basic.png" width="384" height="416"/>
+<tileset name="Basic" tilewidth="32" tileheight="32" tilecount="180" columns="12">
+ <image source="basic.png" width="384" height="480"/>
  <tile id="0">
   <properties>
    <property name="glyph" value="."/>
@@ -46,6 +46,11 @@
    <property name="glyph" value="b"/>
   </properties>
  </tile>
+ <tile id="9">
+  <properties>
+   <property name="glyph" value="#"/>
+  </properties>
+ </tile>
  <tile id="12">
   <properties>
    <property name="glyph" value="w"/>
@@ -59,6 +64,696 @@
  <tile id="14">
   <properties>
    <property name="glyph" value="l"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="ftile" value="floor_grass"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="ftile" value="floor_limestone"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="ftile" value="floor_w_marble"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="ftile" value="floor_pebble"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="ftile" value="floor_dirt"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="ftile" value="floor_hall"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="ftile" value="floor_sand"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="ftile" value="floor_normal"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="ftile" value="floor_slime_acidic"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="ftile" value="floor_black_cobalt"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="ftile" value="floor_slime"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="ftile" value="floor_cage"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="ftile" value="floor_crypt"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="ftile" value="floor_woodground"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="ftile" value="floor_crystal_squares"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="ftile" value="floor_demonic"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="ftile" value="floor_frozen"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="ftile" value="floor_etched"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="ftile" value="floor_infernal"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="ftile" value="floor_vines"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="ftile" value="floor_green_bones"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="ftile" value=""/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="ftile" value="floor_ice"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="ftile" value="floor_infernal"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="ftile" value="floor_iron"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="ftile" value="floor_mesh"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="ftile" value="floor_lair"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="ftile" value="floor_marble"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="ftile" value="floor_mosaic"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="ftile" value="floor_moss"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="ftile" value="floor_mud"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="ftile" value="floor_orc"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="ftile" value="floor_vault"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="ftile" value="floor_rough"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="ftile" value="floor_salt"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="ftile" value="floor_sandstone"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="ftile" value="floor_snake_a"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="ftile" value="floor_swamp"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="ftile" value="floor_tomb"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="ftile" value="floor_volcanic"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="ftile" value=""/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="ftile" value="floor_pebble"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="ftile" value="floor_pebble_brown"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="ftile" value="floor_pebble_blue"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="ftile" value="floor_pebble_green"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="ftile" value="floor_pebble_cyan"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="ftile" value="floor_pebble_red"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="ftile" value="floor_pebble_magenta"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="ftile" value="floor_pebble_darkgray"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="ftile" value="floor_pebble_lightblue"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="ftile" value="floor_pebble_lightgreen"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="ftile" value="floor_pebble_lightcyan"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="ftile" value="floor_pebble_lightred"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="ftile" value="floor_pebble_lightmagenta"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="ftile" value="floor_pebble_yellow"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="ftile" value="floor_pebble_white"/>
+   <property name="glyph" value="."/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="ftile" value="wall_wax"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="ftile" value="wall_church"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="ftile" value="wall_ice_block"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="ftile" value="dngn_metal_wall_white"/>
+   <property name="glyph" value="v"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="ftile" value="wall_volcanic"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="ftile" value="wall_hall"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="ftile" value="WALL_BARS"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="109">
+  <properties>
+   <property name="ftile" value="WALL_BRICK"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="110">
+  <properties>
+   <property name="ftile" value="wall_brick_vines"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="111">
+  <properties>
+   <property name="ftile" value="wall_brick_dark"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="ftile" value="wall_brick_gray"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="ftile" value="wall_catacombs"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="ftile" value="wall_cobalt_rock"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="ftile" value="wall_cobalt_stone"/>
+   <property name="glyph" value="c"/>
+  </properties>
+ </tile>
+ <tile id="116">
+  <properties>
+   <property name="ftile" value="wall_crypt"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="ftile" value="wall_emerald"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="ftile" value="wall_desolation"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="119">
+  <properties>
+   <property name="ftile" value="wall_hell"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="ftile" value="wall_icy_stone"/>
+   <property name="glyph" value="c"/>
+  </properties>
+ </tile>
+ <tile id="121">
+  <properties>
+   <property name="ftile" value="DNGN_METAL_IRON"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="122">
+  <properties>
+   <property name="ftile" value="wall_swamp"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="123">
+  <properties>
+   <property name="ftile" value="wall_marble"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="124">
+  <properties>
+   <property name="ftile" value="dngn_metal_wall_red"/>
+   <property name="glyph" value="v"/>
+  </properties>
+ </tile>
+ <tile id="125">
+  <properties>
+   <property name="ftile" value="wall_orc"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="126">
+  <properties>
+   <property name="ftile" value="wall_pebble_red"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="127">
+  <properties>
+   <property name="ftile" value="wall_permarock"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="ftile" value="wall_hall"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="129">
+  <properties>
+   <property name="ftile" value="wall_sandstone"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="ftile" value="wall_shoals"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="ftile" value="dngn_silver_wall"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="ftile" value="stone_wall_slime"/>
+   <property name="glyph" value="c"/>
+  </properties>
+ </tile>
+ <tile id="133">
+  <properties>
+   <property name="ftile" value="wall_slime"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="ftile" value="wall_snake"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="135">
+  <properties>
+   <property name="ftile" value="wall_spider"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="136">
+  <properties>
+   <property name="ftile" value="wall_stone_dark"/>
+   <property name="glyph" value="c"/>
+  </properties>
+ </tile>
+ <tile id="137">
+  <properties>
+   <property name="ftile" value="dngn_stone_wall"/>
+   <property name="glyph" value="c"/>
+  </properties>
+ </tile>
+ <tile id="138">
+  <properties>
+   <property name="ftile" value="dngn_stone_dark"/>
+   <property name="glyph" value="c"/>
+  </properties>
+ </tile>
+ <tile id="139">
+  <properties>
+   <property name="ftile" value="wall_tomb"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="140">
+  <properties>
+   <property name="ftile" value="wall_undead_brown"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="141">
+  <properties>
+   <property name="ftile" value="wall_undead"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="142">
+  <properties>
+   <property name="ftile" value="stone_wall_vault"/>
+   <property name="glyph" value="c"/>
+  </properties>
+ </tile>
+ <tile id="143">
+  <properties>
+   <property name="ftile" value="wall_vault"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="ftile" value="wall_vines"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="ftile" value="wall_zot"/>
+   <property name="glyph" value="x"/>
+  </properties>
+ </tile>
+ <tile id="156">
+  <properties>
+   <property name="ftile" value="dngn_crystal_blue"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="157">
+  <properties>
+   <property name="ftile" value="dngn_crystal_brown"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="158">
+  <properties>
+   <property name="ftile" value="dngn_crystal_cyan"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="159">
+  <properties>
+   <property name="ftile" value="dngn_crystal_dark_gray"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="ftile" value="dngn_crystal_green"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="ftile" value="dngn_crystal_lightblue"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="ftile" value="dngn_crystal_lightcyan"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="163">
+  <properties>
+   <property name="ftile" value="dngn_crystal_lightgray"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="164">
+  <properties>
+   <property name="ftile" value="dngn_crystal_lightgreen"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="165">
+  <properties>
+   <property name="ftile" value="dngn_crystal_lightmagenta"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="166">
+  <properties>
+   <property name="ftile" value="dngn_crystal_lightred"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="ftile" value="dngn_crystal_magenta"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="ftile" value="dngn_crystal_brown"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name="ftile" value="dngn_crystal_white"/>
+   <property name="glyph" value="b"/>
+  </properties>
+ </tile>
+ <tile id="170">
+  <properties>
+   <property name="ftile" value="dngn_crystal_yellow"/>
+   <property name="glyph" value="b"/>
   </properties>
  </tile>
 </tileset>

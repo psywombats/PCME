5282f93eda815d4492f6f627d73b250fadd710fb
psywombats <psy_wombats@wombatrpgs.net>
2017-06-14 00:45:44 -0700

    Fundamentally flawed kfeat implementation

---
+++ pcme/src/net/wombatrpgs/pcme/DcssTile.java
@@ -0,0 +1,75 @@
+/**
+ *  DcssTile.java
+ *  Created on Jun 13, 2017 10:33:14 PM for project pcme
+ *  Author: psy_wombats
+ *  Contact: psy_wombats@wombatrpgs.net
+ */
+package net.wombatrpgs.pcme;
+
+/**
+ * Info struct for what will result in one glyph to the DCSS map.
+ */
+public class DcssTile {
+
+	protected Character glyph;
+	protected Character kfeatGlyph;
+	
+	/**
+	 * Creates a new tile with the given glyph.
+	 * @param	glyph			The glyph used by this tile, at least initially
+	 */
+	public DcssTile(Character glyph) {
+		this.glyph = glyph;
+	}
+	
+	/**
+	 * Returns the glyph this tile would use if printed immediately. Only returns the topmost glyph,
+	 * so anything unusual underneath will have to be picked up by the kfeat pass.
+	 * @return					The current glyph
+	 */
+	public Character getGlyph() {
+		return glyph;
+	}
+	
+	/**
+	 * Overrides the currrent glyph and places something new in this tile.
+	 * @param	glyph			The new glyph to use
+	 */
+	public void setGlyph(Character glyph) {
+		this.glyph = glyph;
+	}
+	
+	/**
+	 * Attempts to put a glyph on top of this glyph of a different type. Because DCSS has no
+	 * concept of layers, we'll eventually have to use KFEAT to place the lower one. This is used
+	 * for stuff like shrubs in water, monsters on items, etc.
+	 * @param	glyph			The top glyph
+	 */
+	public void addOverlaidGlyph(Character glyph) {
+		if (this.kfeatGlyph != null) {
+			System.err.println("Too many glyphs on tile");
+			return;
+		}
+		this.kfeatGlyph = this.glyph;
+		this.glyph = glyph;
+	}
+	
+	/**
+	 * Returns the underlying glyph that needs to be placed via kfeat, if one exists. If none does,
+	 * returns null.
+	 * @return					The underlying kfeat glyph, or null if none
+	 */
+	public Character getUnderlaidGlyph() {
+		return kfeatGlyph;
+	}
+	
+	/**
+	 * Performs a substitution where this tile is now represented by a new glyph that will be
+	 * subst'd by kfeat at the output step.
+	 * @param	kfeatGlyph		The new glyph for this tile
+	 */
+	public void kfeatSubstitute(Character kfeatGlyph) {
+		this.glyph = kfeatGlyph;
+		this.kfeatGlyph = null;
+	}
+}
--- pcme/src/net/wombatrpgs/pcme/PcmeConverter.java
+++ pcme/src/net/wombatrpgs/pcme/PcmeConverter.java
@@ -8,6 +8,10 @@ package net.wombatrpgs.pcme;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import tiled.core.Map;
 import tiled.core.ObjectGroup;
@@ -21,6 +25,14 @@ import tiled.io.TMXMapReader;
 public class PcmeConverter {
 	
 	protected File inputFile;
+	protected Map map;
+	protected String result;
+	
+	protected HashSet<Character> unusedGlyphs;
+	protected HashMap<String, ArrayList<DcssTile>> kfeatTiles;
+	protected DcssTile[][] dcssMap;
+	protected HashMap<Character, ArrayList<DcssTile>> tilesByGlyph;
+	protected HashMap<String, Character> kfeatSubs;
 	
 	/**
 	 * Creates a new conversion job for a given file.
@@ -36,55 +48,65 @@ public class PcmeConverter {
 	 */
 	public String convertToDcssString() {
 		TMXMapReader reader = new TMXMapReader();
-		Map map = null;
+		map = null;
 		try {
 			map = reader.readMap(inputFile.getAbsolutePath());
 		} catch (Exception e) {
 			reportError("TMX error");
 			e.printStackTrace();
-			return "";
+			return null;
 		}
 		
 		if (map.getLayerCount() != 3) {
 			reportError("Expecting 3 layers, found " + map.getLayerCount());
-			return "";
+			return null;
 		}
 		if (!TileLayer.class.isAssignableFrom(map.getLayer(0).getClass())) {
 			reportError("Tile layer bad type " + map.getLayer(0).getClass());
-			return "";
+			return null;
 		}
 		if (!TileLayer.class.isAssignableFrom(map.getLayer(1).getClass())) {
 			reportError("Feature layer bad type " + map.getLayer(1).getClass());
-			return "";
+			return null;
 		}
 		if (!ObjectGroup.class.isAssignableFrom(map.getLayer(2).getClass())) {
 			reportError("Object layer bad type " + map.getLayer(2).getClass());
-			return "";
+			return null;
 		}
 		TileLayer tileLayer = (TileLayer) map.getLayer(0);
 		TileLayer featureLayer = (TileLayer) map.getLayer(1);
 		ObjectGroup objectLayer = (ObjectGroup) map.getLayer(2);
 		
-		ArrayList<ArrayList<Character>> glyphs = new ArrayList<ArrayList<Character>>();
+		String characterString = "`~!&-_:;\"'pqrsuyzDEFHJKLMNOQRSTZ?";
+		unusedGlyphs = new HashSet<Character>();
+		for (int i = 0; i < characterString.length(); i += 1) {
+			unusedGlyphs.add(characterString.charAt(i));
+		}
+		
+		// Basic layer pass
+		dcssMap = new DcssTile[tileLayer.getHeight()][tileLayer.getWidth()];
+		tilesByGlyph = new HashMap<>();
 		for (int y = 0; y < tileLayer.getHeight(); y += 1) {
-			glyphs.add(new ArrayList<Character>());
 			for (int x = 0; x < tileLayer.getWidth(); x += 1) {
 				Tile tile = tileLayer.getTileAt(x, y);
 				
 				// blank space
 				if (tile == null) {
-					glyphs.get(y).add(' ');
+					dcssMap[y][x] = new DcssTile(' ');
 					continue;
 				}
 				
 				// raw glyph
-				String glyph = tile.getProperties().getProperty("glyph");
-				if (glyph != null) {
-					if (glyph.length() != 1) {
-						reportError("Bad glyph in tile " + tile + ": " + glyph);
-						glyphs.get(y).add('.');
+				String glyphString = tile.getProperties().getProperty("glyph");
+				if (glyphString != null) {
+					if (glyphString.length() != 1) {
+						reportError("Bad glyph in tile " + tile + ": " + glyphString);
+						return null;
 					} else {
-						glyphs.get(y).add(glyph.charAt(0));
+						Character glyph = glyphString.charAt(0);
+						dcssMap[y][x] = new DcssTile(glyph);
+						unusedGlyphs.remove(glyph);
+						registerTileGlyph(dcssMap[y][x]);
 					}
 				}
 				
@@ -92,6 +114,8 @@ public class PcmeConverter {
 			}
 		}
 		
+		// Feature layer pass
+		kfeatTiles = new HashMap<String, ArrayList<DcssTile>>();
 		for (int y = 0; y < featureLayer.getHeight(); y += 1) {
 			for (int x = 0; x < featureLayer.getHeight(); x += 1) {
 				Tile tile = featureLayer.getTileAt(x, y);
@@ -99,20 +123,52 @@ public class PcmeConverter {
 					continue;
 				}
 				
-				Character existingGlyph = glyphs.get(y).get(x);
+				DcssTile existingTile = dcssMap[y][x];
+				Character existingGlyph = existingTile.getGlyph();
 				
-				String glyph = tile.getProperties().getProperty("glyph");
-				if (glyph != null) {
-					if (glyph.length() != 1) {
-						reportError("Bad glyph in tile " + tile + ": " + glyph);
+				String glyphString = tile.getProperties().getProperty("glyph");
+				if (glyphString != null) {
+					if (glyphString.length() != 1) {
+						reportError("Bad glyph in tile " + tile + ": " + glyphString);
+						continue;
+					}
+					Character glyph = glyphString.charAt(0);
+					unusedGlyphs.remove(glyph);
+					if (existingGlyph == ' ' || existingGlyph == '.' || existingGlyph == glyph) {
+						existingTile.setGlyph(glyph);
 					} else {
-						glyphs.get(y).set(x, glyph.charAt(0));
+						// an odd glyph has been placed above some terrain
+						unregisterTileGlyph(existingTile);
+						existingTile.addOverlaidGlyph(glyph);
+						registerTileGlyph(existingTile);
+						registerKfeatTile(existingTile);
 					}
 				}
 			}
 		}
 		
-		String result = "";
+		// KFEAT fill-in-the-blank pass
+		// format is "xy" -> 'z' where z represents x lying on top of y
+		kfeatSubs = new HashMap<String, Character>();
+		for (String kfeatKey : kfeatTiles.keySet()) {
+			ArrayList<DcssTile> subbedTiles = kfeatTiles.get(kfeatKey);
+			DcssTile sampleTile = subbedTiles.get(0);
+			if (completelyCoveredByKfeat(sampleTile.getGlyph())) {
+				// we don't need to introduce a new glyph because all of them will be transformed
+				kfeatSubs.put(kfeatKey, sampleTile.getGlyph());
+			} else {
+				// some have the underlying feature, some don't
+				Character subChar = (Character)unusedGlyphs.toArray()[0];
+				unusedGlyphs.remove(subChar);
+				kfeatSubs.put(kfeatKey, sampleTile.getGlyph());
+				for (DcssTile tile : subbedTiles) {
+					tile.kfeatSubstitute(subChar);
+				}
+			}
+		}
+		
+		// Basic headers
+		result = "";
 		result += "##############################################################################";
 		result += "\n";
 		result += "NAME:       ";
@@ -123,20 +179,36 @@ public class PcmeConverter {
 					map.getFilename().indexOf('.'));
 		}
 		result += "\n";
-		result = appendPropertyIfExists(result, map, "place");
-		result = appendPropertyIfExists(result, map, "tags");
-		result = appendPropertyIfExists(result, map, "weight");
-		result = appendPropertyIfExists(result, map, "chance");
-		result = appendPropertyIfExists(result, map, "orient");
+		appendPropertyIfExists("place");
+		appendPropertyIfExists("tags");
+		appendPropertyIfExists("weight");
+		appendPropertyIfExists("chance");
+		appendPropertyIfExists("orient");
 		if (map.getProperties().getProperty("code") != null) {
 			result += map.getProperties().getProperty("code") + "\n";
 		}
 		
+		// Kfeat
+		for (String kfeatKey : kfeatSubs.keySet()) {
+			Character subChar = kfeatSubs.get(kfeatKey);
+			Character bottomGlyph = kfeatKey.charAt(0);
+			Character topGlyph = kfeatKey.charAt(1);
+			appendCode("kfeat", subChar + " : " + bottomGlyph);
+			// TODO: handle the top character
+			// I think eventually we'll need to parse $, d, and 1 etc to mons and items
+			// then kfeat those in
+			// At the moment this code just nukes the feature layer entirely
+			if (!kfeatKey.endsWith("" + subChar)) {
+				// non-uniform transformation
+			}
+		}
+		
+		// Map
 		result += "MAP\n";
-		for (ArrayList<Character> row : glyphs) {
+		for (int y = 0; y < featureLayer.getHeight(); y += 1) {
 			String line = "";
-			for (Character glyph : row) {
-				line += glyph;
+			for (int x = 0; x < featureLayer.getHeight(); x += 1) {
+				line += dcssMap[y][x].getGlyph();
 			}
 			result += line + "\n";
 		}
@@ -157,12 +229,9 @@ public class PcmeConverter {
 	 * If a property with the given name exists, appends it to the result string. The property name
 	 * is automatically converted to uppercase in the result string, but is case-insensitive in the
 	 * reading.
-	 * @param	result			The current result string
-	 * @param	map				The map with the properties to read from
 	 * @param	propertyName	The name of the property to append
-	 * @return					The result string possibly with the property value appended
 	 */
-	protected String appendPropertyIfExists(String result, Map map, String propertyName) {
+	protected void appendPropertyIfExists(String propertyName) {
 		String value = null;
 		if (map.getProperties().getProperty(propertyName.toLowerCase()) != null) {
 			value = map.getProperties().getProperty(propertyName.toLowerCase());
@@ -170,14 +239,68 @@ public class PcmeConverter {
 			value = map.getProperties().getProperty(propertyName.toUpperCase());
 		}
 		if (value != null) {
-			String newResult = result + propertyName.toUpperCase() + ":";
-			for (int i = propertyName.length(); i < 12 - 1; i += 1) {
-				newResult += " ";
-			}
-			newResult += value + "\n";
-			return newResult;
-		} else {
-			return result;
+			appendCode(propertyName, value);
 		}
+	}
+	
+	/**
+	 * Appends a code line to the result in the format COMMAND:STUFF, except it fills in proper
+	 * whitespace after the colon. Uppercases the command.
+	 * @param	command			The command name preceding the colon
+	 * @param	code			The code line following the colon
+	 */
+	protected void appendCode(String command, String code) {
+		result += command.toUpperCase() + ":";
+		for (int i = code.length(); i < 12 - 1; i += 1) {
+			result += " ";
+		}
+		result += code + "\n";
+	}
+	
+	/**
+	 * Given a tile, adds it to the tilesByGlyph map.
+	 * @param	tile			The tile to add to the map
+	 */
+	protected void registerTileGlyph(DcssTile tile) {
+		ArrayList<DcssTile> sameTile = tilesByGlyph.get(tile.getGlyph());
+		if (sameTile == null) {
+			sameTile = new ArrayList<DcssTile>();
+			tilesByGlyph.put(tile.getGlyph(), sameTile);
+		}
+		sameTile.add(tile);
+	}
+	
+	/**
+	 * Given a tile, removes it from the tilesByGlyph map.
+	 * @param	tile			The tile to remove from the map
+	 */
+	protected void unregisterTileGlyph(DcssTile tile) {
+		tilesByGlyph.get(tile.getGlyph()).remove(tile);
+	}
+	
+	/**
+	 * Given a glyph, checks if every glyph of that type will have the same kfeat transformation
+	 * applied to it. Basically, if every thing of gold is put in water, this will return true,
+	 * but if half the gold's on dry ground, it returns false.
+	 * @param	glyph			The glyph to check
+	 * @return					True if a uniform transformation will be applied
+	 */
+	protected boolean completelyCoveredByKfeat(Character glyph) {
+		return kfeatTiles.get(glyph).size() == tilesByGlyph.get(glyph).size(); 
+	}
+	
+	/**
+	 * Registers a given tile in the kfeat list. Assumes the tile has already had its kfeat tile
+	 * set.
+	 * @param	tile			The tile to register
+	 */
+	protected void registerKfeatTile(DcssTile tile) {
+		String key = "" + tile.getUnderlaidGlyph() + tile.getGlyph();
+		ArrayList<DcssTile> sameSub = kfeatTiles.get(key);
+		if (sameSub == null) {
+			sameSub = new ArrayList<DcssTile>();
+			kfeatTiles.put(key, sameSub);
+		}
+		sameSub.add(tile);
 	}
 }
